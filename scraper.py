bf0d0101bf70681ffef711af7d88181acba2beee
zgfif <zizhag@gmail.com>
2024-11-07 16:42:33 +0200

    initial commit

---
+++ scraper.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+
+URL = 'https://itc.ua/'
+
+response = requests.get(URL)
+bs = BeautifulSoup(response.text, 'html.parser')
+
+main = bs.main
+
+news = main.find_all('div', class_='category-news')
+
+for post in news:
+    print('title: ', post.h2.text.strip())
+    print('href: ', post.h2.a['href'])
+    entry_header = post.find(class_='entry-header')
+    print('category:', entry_header.div.span.text.strip())
+
+    print('Date: ', entry_header.find('span', class_='date').text.strip())
+    print('')
